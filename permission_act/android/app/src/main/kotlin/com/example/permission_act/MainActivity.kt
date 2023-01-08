2cb060274008c4f4de97e66f9019987cf2f9a3f4
Alliana Marie <allianaonate@gmail.com>
2023-01-09 00:58:03 +0800

    lacking permission for gallery

---
+++ permission_act/android/app/src/main/kotlin/com/example/permission_act/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.permission_act
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ permission_act/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:permission_act/home_screen.dart';
+
+void main(){
+  runApp(MaterialApp(
+    theme: ThemeData(
+      primarySwatch: Colors.pink
+    ),
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
+  ));
+}
